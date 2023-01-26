769a32b72b6faa2a3feb00295571bfececb02530
AmandaRiosAmaral <amandariosamaral@gmail.com>
2023-01-26 16:54:58 +0100

    first commit

---
+++ PhotoApp/PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by Amanda Rios Amaral on 26/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
